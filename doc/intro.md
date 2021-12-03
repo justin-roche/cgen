8455cf72eda6f713ac8f571ed335aa1b5a8fb3eb
Justin Roche <justinroche@Justins-Mini.lan>
2021-12-02 20:29:36 -0500

    switch to clj

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cgen
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cgen/core_test.clj
@@ -0,0 +1,7 @@
+(ns cgen.core-test
+  (:require [clojure.test :refer :all]
+            [cgen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
